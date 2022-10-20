966e9f1ece5f5248b9402e18d1ee6f470f75eaee
Krysztal <suibing112233@outlook.com>
2022-10-20 23:47:30 +0800

    Port FillCreateItemGroupEvent to fabric flavor event.

--- src/main/java/plus/dragons/createdragonlib/mixin/CreateItemGroupBaseMixin.java
+++ src/main/java/plus/dragons/createdragonlib/mixin/CreateItemGroupBaseMixin.java
@@ -2,8 +2,8 @@ package plus.dragons.createdragonlib.mixin;
 
 import com.simibubi.create.foundation.item.CreateItemGroupBase;
 import net.minecraft.core.NonNullList;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,12 +12,22 @@ import plus.dragons.createdragonlib.api.event.FillCreateItemGroupEvent;
 
 @Mixin(CreateItemGroupBase.class)
 public class CreateItemGroupBaseMixin {
-    
+
     @Inject(method = "fillItemList", at = @At("TAIL"))
     private void postFillCreateItemGroupEvent(NonNullList<ItemStack> items, CallbackInfo ci) {
-        var event = new FillCreateItemGroupEvent((CreateItemGroupBase) (Object) this, items);
-        MinecraftForge.EVENT_BUS.post(event);
-        event.apply();
+//        var event = new FillCreateItemGroupEvent((CreateItemGroupBase) (Object) this, items);
+//        MinecraftForge.EVENT_BUS.post(event);
+//        event.apply();
+
+        InteractionResult result = FillCreateItemGroupEvent
+                .FillCreateItemGroupCallBack
+                .EVENT
+                .invoker()
+                .interact((CreateItemGroupBase) (Object) this, items);
+
+        if (result == InteractionResult.FAIL) {
+            ci.cancel();
+        }
     }
-    
+
 }
