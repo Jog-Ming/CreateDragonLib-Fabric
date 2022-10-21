70f86c73f1196fb0c25ea46043cfd444273adc86
Krysztal <suibing112233@outlook.com>
2022-10-21 15:43:30 +0800

    Fix errors in RegistrateHelper - Make `NoColorFluidAttributes` deprecated. - Fix method in builder of registrate.

--- src/main/java/plus/dragons/createdragonlib/entry/RegistrateHelper.java
+++ src/main/java/plus/dragons/createdragonlib/entry/RegistrateHelper.java
@@ -9,6 +9,9 @@ import com.tterrag.registrate.util.entry.MenuEntry;
 import com.tterrag.registrate.util.nullness.NonNullConsumer;
 import com.tterrag.registrate.util.nullness.NonNullFunction;
 import com.tterrag.registrate.util.nullness.NonNullSupplier;
+import io.github.fabricators_of_create.porting_lib.util.FluidStack;
+import io.github.fabricators_of_create.porting_lib.util.SimpleFlowableFluid;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.MenuAccess;
 import net.minecraft.client.renderer.entity.EntityRenderer;
@@ -19,20 +22,22 @@ import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.material.FluidState;
-import net.minecraftforge.fluids.FluidStack;
+
+import static net.minecraft.client.renderer.block.model.BlockElementFace.NO_TINT;
+
 
 public class RegistrateHelper {
     public static class EntityType{
         public static <T extends net.minecraft.world.entity.Entity> CreateEntityBuilder<T, ?> register(CreateRegistrate registrate, String name, net.minecraft.world.entity.EntityType.EntityFactory<T> factory,
                                                                                                         NonNullSupplier<NonNullFunction<EntityRendererProvider.Context, EntityRenderer<? super T>>> renderer,
                                                                                                         MobCategory group, int range, int updateFrequency, boolean sendVelocity, boolean immuneToFire,
-                                                                                                        NonNullConsumer<net.minecraft.world.entity.EntityType.Builder<T>> propertyBuilder) {
+                                                                                                        NonNullConsumer<FabricEntityTypeBuilder<T>> propertyBuilder) {
             String id = Lang.asId(name);
             return (CreateEntityBuilder<T, ?>) registrate
                     .entity(id, factory, group)
-                    .properties(b -> b.setTrackingRange(range)
-                            .setUpdateInterval(updateFrequency)
-                            .setShouldReceiveVelocityUpdates(sendVelocity))
+                    .properties(b -> b.trackRangeBlocks( range)
+                            .trackedUpdateRate(updateFrequency)
+                            .forceTrackedVelocityUpdates(sendVelocity))
                     .properties(propertyBuilder)
                     .properties(b -> {
                         if (immuneToFire)
@@ -56,19 +61,20 @@ public class RegistrateHelper {
          * Removing alpha from tint prevents optifine from forcibly applying biome
          * colors to modded fluids (Makes translucent fluids disappear)
          */
-        public static class NoColorFluidAttributes extends AllFluids.TintedFluidType {
+        @Deprecated
+        public static class NoColorFluidAttributes   {
 
-            public NoColorFluidAttributes(Properties properties, ResourceLocation stillTexture,
+            public NoColorFluidAttributes(SimpleFlowableFluid.Properties properties, ResourceLocation stillTexture,
                                           ResourceLocation flowingTexture) {
-                super(properties, stillTexture, flowingTexture);
+//                super(properties, stillTexture, flowingTexture);
             }
 
-            @Override
+
             protected int getTintColor(FluidStack stack) {
                 return NO_TINT;
             }
 
-            @Override
+
             public int getTintColor(FluidState state, BlockAndTintGetter world, BlockPos pos) {
                 return 0x00ffffff;
             }
